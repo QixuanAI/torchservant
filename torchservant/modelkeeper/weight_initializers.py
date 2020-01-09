5a772db3486401b50e91f7a2d8126b556cf0e67b
QixuanAI <qxsoftware@163.com>
2020-01-09 23:17:38 +0800

    add cfgenator as ConfigGenerator

---
+++ torchservant/modelkeeper/weight_initializers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/01/07 23:43
+# @Author  : LQX
+# @Email   : qxsoftware@163.com
+# @File    : weight_initializers.py
+# @Software: PyCharm
+
+import os
+
+
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parse = argparse.ArgumentParser()
+    args = parse.parse_args()
+    main(args)
